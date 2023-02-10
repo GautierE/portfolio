7eb2d3302489316564fb0d8bca3b9c5c135930d9
Gautier <gautier.edel66@gmail.com>
2023-02-10 08:28:02 +0100

    Fixed image position welcome page

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Welcome() {
   return (
     <div class="flex h-screen w-screen items-center bg-mint">
-      <div class="relative mx-auto h-3/5 w-7/12">
+      <div class="mx-auto h-1/6 w-7/12">
         <h1 class=" bordered-text sticky z-10 text-8xl text-white">
           Hi, my name is <span class="font-bold text-navy">Gautier</span>.
         </h1>
@@ -12,16 +12,16 @@ export default function Welcome() {
           I&apos;m a <span class="text-navy">fullstack</span> web and mobile{" "}
           <br /> developer
         </p>
-        <Image
-          class="right-100 absolute top-20 right-0 z-0 rounded-full"
-          src={"/Picsou.png"}
-          alt={"Profile picture"}
-          width={648 / 1.2}
-          height={533 / 1.2}
-          quality={100}
-          priority={true}
-        />
       </div>
+      <Image
+        class="absolute right-80 top-6 z-0 rounded-full"
+        src={"/Picsou.png"}
+        alt={"Profile picture"}
+        width={648 / 1.4}
+        height={533 / 1.4}
+        quality={100}
+        priority={true}
+      />
     </div>
   );
 }
