5a5433c7963254943d76dcf8f178b443844185cb
Gautier <gautier.edel.info@gmail.com>
2023-07-13 23:36:07 +0200

    [WIP] Rework skills section & added new skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -3,24 +3,39 @@ import { motion, useInView, useAnimation } from "framer-motion";
 import htmlLogo from "../../public/skills/html_logo.svg";
 import cssLogo from "../../public/skills/css_logo.svg";
 import jsLogo from "../../public/skills/js_logo.svg";
+import typescriptLogo from "../../public/skills/typescript_logo.svg";
 import nextjsLogo from "../../public/skills/nextjs_logo.svg";
 import reactLogo from "../../public/skills/react_logo.svg";
 import jqueryLogo from "../../public/skills/jquery_logo.svg";
 import reactQueryLogo from "../../public/skills/react_query_logo.svg";
+import angularLogo from "../../public/skills/angular_logo.svg";
+import jasmineLogo from "../../public/skills/jasmine_logo.svg";
+import karmaLogo from "../../public/skills/karma_logo.svg";
 import tailwindLogo from "../../public/skills/tailwind_logo.svg";
 import bootstrapLogo from "../../public/skills/bootstrap_logo.svg";
 import framermotionLogo from "../../public/skills/framermotion_logo.webp";
 import phpLogo from "../../public/skills/php_logo.svg";
 import symfonyLogo from "../../public/skills/symfony_logo.svg";
+import laravelLogo from "../../public/skills/laravel_logo.svg";
 import doctrineLogo from "../../public/skills/doctrine_logo.svg";
+import composerLogo from "../../public/skills/composer_logo.svg";
 import nodeLogo from "../../public/skills/node_logo.svg";
 import expressLogo from "../../public/skills/express_logo.svg";
 import sequelizeLogo from "../../public/skills/sequelize_logo.svg";
 import mysqlLogo from "../../public/skills/mysql_logo.svg";
 import sqliteLogo from "../../public/skills/sqlite_logo.svg";
+import cassandraLogo from "../../public/skills/cassandra_logo.svg";
+import firebaseLogo from "../../public/skills/firebase_logo.svg";
+import gcpLogo from "../../public/skills/googlecloud_logo.svg";
+import vercelLogo from "../../public/skills/vercel_logo.svg";
 import gitLogo from "../../public/skills/git_logo.svg";
+import githubLogo from "../../public/skills/github_logo.svg";
+import gitlabLogo from "../../public/skills/gitlab_logo.svg";
+import dockerLogo from "../../public/skills/docker_logo.svg";
+import bashLogo from "../../public/skills/bash_logo.svg";
+import jiraLogo from "../../public/skills/jira_logo.svg";
+import trelloLogo from "../../public/skills/trello_logo.svg";
 import agileIcon from "../../public/skills/agile_icon.svg";
-import scrumIcon from "../../public/skills/scrum_icon.svg";
 import chromeLogo from "../../public/skills/chrome_logo.svg";
 import webscrappingIcon from "../../public/skills/webscrapping_icon.svg";
 import oauthLogo from "../../public/skills/oauth_logo.svg";
@@ -39,35 +54,60 @@ export default function Skills() {
     }
   }, [isInView, controls]);
 
-  const frontEndLogos = [
+  const frontEndBasicLogos = [
     { url: htmlLogo, desc: "HTML" },
     { url: cssLogo, desc: "CSS" },
     { url: jsLogo, desc: "Javascript" },
-    { url: nextjsLogo, desc: "Next.js" },
-    { url: reactLogo, desc: "React" },
-    { url: jqueryLogo, desc: "jQuery" },
-    { url: reactQueryLogo, desc: "React Query" },
+    { url: typescriptLogo, desc: "Typescript" },
     { url: tailwindLogo, desc: "Tailwind CSS" },
     { url: bootstrapLogo, desc: "Bootstrap" },
+  ];
+
+  const frontEndLibLogos = [
+    { url: nextjsLogo, desc: "Next.js" },
+    { url: reactLogo, desc: "React" },
+    { url: angularLogo, desc: "Angular" },
+    { url: jqueryLogo, desc: "jQuery" },
+    { url: reactQueryLogo, desc: "React Query" },
     { url: framermotionLogo, desc: "Framer Motion" },
+    { url: jasmineLogo, desc: "Jasmine" },
+    { url: karmaLogo, desc: "Karma" },
     { url: reactLogo, desc: "React Native" },
   ];
 
   const backEndLogos = [
     { url: phpLogo, desc: "PHP" },
     { url: symfonyLogo, desc: "Symfony" },
+    { url: laravelLogo, desc: "Laravel" },
     { url: doctrineLogo, desc: "Doctrine" },
+    { url: composerLogo, desc: "Composer" },
     { url: nodeLogo, desc: "Node.js" },
     { url: expressLogo, desc: "Express.js" },
     { url: sequelizeLogo, desc: "Sequelize" },
+  ];
+
+  const databasesLogos = [
     { url: mysqlLogo, desc: "MySQL" },
     { url: sqliteLogo, desc: "SQLite" },
+    { url: cassandraLogo, desc: "Cassandra" },
+    { url: firebaseLogo, desc: "Firestore" },
+  ];
+
+  const devopsLogos = [
+    { url: gcpLogo, desc: "GCP" },
+    { url: firebaseLogo, desc: "Firebase" },
+    { url: vercelLogo, desc: "Vercel" },
+    { url: gitLogo, desc: "Git" },
+    { url: gitlabLogo, desc: "GitLab" },
+    { url: githubLogo, desc: "GitHub Actions" },
+    { url: dockerLogo, desc: "Docker" },
+    { url: bashLogo, desc: "Bash" },
+    { url: jiraLogo, desc: "Jira" },
+    { url: trelloLogo, desc: "Trello" },
   ];
 
   const miscsLogos = [
-    { url: gitLogo, desc: "Git" },
     { url: agileIcon, desc: "Agile" },
-    { url: scrumIcon, desc: "Scrum" },
     { url: chromeLogo, desc: "Chrome extension" },
     {
       url: webscrappingIcon,
@@ -127,6 +167,21 @@ export default function Skills() {
     },
   };
 
+  // Animation properties
+  const transition = {
+    loop: Infinity,
+    ease: "linear",
+    duration: 2, // Adjust the duration as needed
+  };
+
+  // Animate the container on the X-axis
+  const containerVariants = {
+    animate: {
+      x: "-99999999999999999999999999999999999999999999999999999%", // Adjust the distance based on your container width
+      transition,
+    },
+  };
+
   return (
     <div className="sticky flex flex-col items-center justify-center h-screen xl:bg-purple-white bg-purple tall:top-0">
       <motion.h2
@@ -138,165 +193,120 @@ export default function Skills() {
       >
         {t("skills.title")}
       </motion.h2>
-      <div className="flex h-[90%] w-[90%] flex-col justify-evenly overflow-x-hidden rounded-md bg-white px-2 shadow md:h-4/5 xl:top-[17%] xl:w-3/4 xl:justify-evenly xl:p-5">
+      <div className="flex h-[90%] w-[90%] flex-col justify-evenly overflow-x-hidden rounded-md bg-white px-2 shadow md:h-[90%] xl:top-[10%] xl:w-3/4 xl:justify-evenly xl:p-5">
         <div>
-          <motion.p
-            className="pt-2 pl-2 text-lg xl:pb-2 xl:pt-0 xl:text-2xl"
-            variants={{
-              hidden: { y: -30, opacity: 0 },
-              visible: {
-                y: 0,
-                opacity: 1,
-                transition: {
-                  delay: 0.5,
-                  duration: 1,
-                  ease: "easeInOut",
-                },
-              },
-            }}
-            initial="hidden"
-            animate={controls}
-          >
-            Front-end
+          <motion.p className="pt-2 pl-2 text-lg xl:pb-2 xl:pt-0 xl:text-2xl">
+            Front-end basic &amp; Styling
           </motion.p>
-          <motion.div
-            className="flex h-[16vh] flex-col flex-wrap items-center justify-evenly overflow-y-hidden overflow-x-scroll rounded-lg border-2 border-purple bg-white md:h-[22vh] xl:h-[100px] xl:flex-row xl:flex-nowrap xl:overflow-x-auto xl:rounded-full"
-            variants={expandSkills}
-            custom={{ leftToRight: true, delay: 0 }}
-            initial="hidden"
-            animate={controls}
-          >
-            {frontEndLogos.map((item, i) => (
-              <motion.div
-                key={i}
-                className="flex w-[135px] flex-col items-center justify-center xl:h-full xl:w-full xl:justify-evenly"
-                variants={drawImages}
-                custom={{
-                  i: i,
-                  waitTime: 0,
-                  itemCount: frontEndLogos.length - 1,
-                  leftToRight: true,
-                }}
-              >
-                <motion.img
-                  variants={drawImages}
-                  custom={{
-                    i: i,
-                    waitTime: 0,
-                    itemCount: frontEndLogos.length - 1,
-                    leftToRight: true,
-                  }}
-                  src={item.url?.src}
-                  alt={item.desc}
-                  width={"55vw"}
-                  className="p-2 border-2 rounded-full border-purple"
-                />
-                <p className="whitespace-nowrap lg:text-[1.5vh]">{item.desc}</p>
-              </motion.div>
-            ))}
-          </motion.div>
+          <SkillRow rowSkills={frontEndBasicLogos} />
         </div>
         <div>
-          <motion.p
-            className="pt-2 pl-2 text-lg xl:pb-2 xl:pt-0 xl:text-2xl"
-            variants={{
-              hidden: { y: -30, opacity: 0 },
-              visible: {
-                y: 0,
-                opacity: 1,
-                transition: {
-                  delay: 1.5,
-                  duration: 1,
-                  ease: "easeInOut",
-                },
-              },
-            }}
-            initial="hidden"
-            animate={controls}
-          >
-            Back-end
+          <motion.p className="pt-2 pl-2 text-lg xl:pb-2 xl:pt-0 xl:text-2xl">
+            Front-end Librairies &amp; Frameworks
           </motion.p>
-          <motion.div
-            className="flex h-[16vh] flex-col flex-wrap items-center justify-evenly overflow-y-hidden overflow-x-scroll rounded-lg border-2 border-purple bg-white md:h-[22vh] xl:h-[100px] xl:flex-row xl:flex-nowrap xl:overflow-x-auto xl:rounded-full"
-            variants={expandSkills}
-            initial="hidden"
-            animate={controls}
-            custom={{ leftToRight: false, delay: 0.5 }}
-          >
-            {backEndLogos.map((item, i) => (
-              <div
-                key={i}
-                className="flex w-[135px] flex-col items-center justify-center xl:h-full xl:w-full xl:justify-evenly"
-              >
-                <motion.img
-                  variants={drawImages}
-                  custom={{
-                    i: i,
-                    waitTime: 1,
-                    itemCount: backEndLogos.length - 1,
-                    leftToRight: false,
-                  }}
-                  src={item.url?.src}
-                  alt={item.desc}
-                  width={"55vw"}
-                  className="p-2 border-2 rounded-full border-purple"
-                />
-                <p className="whitespace-nowrap lg:text-[1.5vh]">{item.desc}</p>
-              </div>
-            ))}
-          </motion.div>
+          <SkillRow rowSkills={frontEndLibLogos} />
+        </div>
+        <div className="flex flex-wrap justify-between w-full">
+          <div className="mr-2">
+            <motion.p className="pt-2 pl-2 text-lg xl:pb-2 xl:pt-0 xl:text-2xl">
+              Back-end
+            </motion.p>
+            <HalfSkillRow rowSkills={backEndLogos} />
+          </div>
+          <div className="ml-2">
+            <motion.p className="pt-2 pr-2 text-lg text-end xl:pb-2 xl:pt-0 xl:text-2xl">
+              Databases
+            </motion.p>
+            <HalfSkillRow rowSkills={databasesLogos} />
+          </div>
         </div>
         <div>
-          <motion.p
-            className="pt-2 pl-2 text-lg xl:pb-2 xl:pt-0 xl:text-2xl"
-            variants={{
-              hidden: { y: -30, opacity: 0 },
-              visible: {
-                y: 0,
-                opacity: 1,
-                transition: {
-                  delay: 2.5,
-                  duration: 1,
-                  ease: "easeInOut",
-                },
-              },
-            }}
-            initial="hidden"
-            animate={controls}
-          >
+          <motion.p className="pt-2 pl-2 text-lg xl:pb-2 xl:pt-0 xl:text-2xl">
+            Dev ops
+          </motion.p>
+          <SkillRow rowSkills={devopsLogos} />
+        </div>
+        <div>
+          <motion.p className="pt-2 pl-2 text-lg xl:pb-2 xl:pt-0 xl:text-2xl">
             {t("skills.miscstext")}
           </motion.p>
-          <motion.div
-            className="flex h-[16vh] flex-col flex-wrap items-center justify-evenly overflow-y-hidden overflow-x-scroll rounded-lg border-2 border-purple bg-white md:h-[22vh] xl:h-[100px] xl:flex-row xl:flex-nowrap xl:overflow-x-auto xl:rounded-full"
-            variants={expandSkills}
-            initial="hidden"
-            animate={controls}
-            custom={{ leftToRight: true, delay: 1 }}
-          >
-            {miscsLogos.map((item, i) => (
-              <div
-                key={i}
-                className="flex w-[135px] flex-col items-center justify-center xl:h-full xl:w-full xl:justify-evenly"
-              >
-                <motion.img
-                  variants={drawImages}
-                  custom={{
-                    i: i,
-                    waitTime: 0,
-                    itemCount: miscsLogos.length - 1,
-                    leftToRight: true,
-                  }}
-                  src={item.url?.src}
-                  alt={item.desc}
-                  width={"55vw"}
-                  className="p-2 border-2 rounded-full border-purple"
-                />
-                <p className="whitespace-nowrap lg:text-[1.5vh]">{item.desc}</p>
-              </div>
-            ))}
-          </motion.div>
+          <SkillRow rowSkills={miscsLogos} />
         </div>
       </div>
     </div>
   );
 }
+
+const SkillRow = ({ rowSkills }) => {
+  // Generate a random initial position between -500 and -1000
+  const initialPosition = Math.floor(Math.random() * (500 - 1000 + 1)) - 1000;
+
+  // Generate a random duration between 20 and 30
+  const duration = Math.floor(Math.random() * 11) + 20;
+
+  return (
+    <motion.div
+      animate={{ x: [initialPosition, -200] }}
+      transition={{
+        duration: duration,
+        repeat: Infinity,
+        ease: "linear",
+        repeatType: "mirror",
+      }}
+      className="flex h-[16vh] flex-col flex-wrap items-center justify-evenly overflow-y-hidden rounded-lg border-2 border-purple bg-white md:h-[22vh] xl:h-[100px] xl:flex-row xl:flex-nowrap xl:overflow-x-auto"
+      style={{ width: `${rowSkills.length * 40}vw` }}
+    >
+      {Array.from({ length: 4 }, () => rowSkills).map((frontEndBasicLogos) =>
+        frontEndBasicLogos.map((item, i) => (
+          <div
+            key={i}
+            className="flex w-[135px] flex-col items-center justify-center xl:h-full xl:w-full xl:justify-evenly"
+          >
+            <img
+              src={item.url?.src}
+              alt={item.desc}
+              width={"55vw"}
+              className="p-2 border-2 rounded-full border-purple"
+            />
+            <p className="whitespace-nowrap lg:text-[1.5vh]">{item.desc}</p>
+          </div>
+        ))
+      )}
+    </motion.div>
+  );
+};
+
+const HalfSkillRow = ({ rowSkills }) => {
+  return (
+    <div className="h-[16vh] min-w-[35vw] max-w-[35vw] overflow-x-hidden overflow-y-hidden rounded-lg border-2 border-purple bg-white md:h-[22vh] xl:h-[100px]">
+      <motion.div
+        animate={{ x: [-2500, -100] }}
+        transition={{
+          duration: 20,
+          repeat: Infinity,
+          ease: "linear",
+          repeatType: "mirror",
+        }}
+        className="flex flex-col items-center h-full justify-evenly xl:flex-row"
+        style={{ width: `${rowSkills.length * 40}vw` }}
+      >
+        {Array.from({ length: 4 }, () => rowSkills).map((frontEndBasicLogos) =>
+          frontEndBasicLogos.map((item, i) => (
+            <div
+              key={i}
+              className="flex w-[135px] flex-col items-center justify-center xl:h-full xl:w-full xl:justify-evenly"
+            >
+              <img
+                src={item.url?.src}
+                alt={item.desc}
+                width={"55vw"}
+                className="p-2 border-2 rounded-full border-purple"
+              />
+              <p className="whitespace-nowrap lg:text-[1.5vh]">{item.desc}</p>
+            </div>
+          ))
+        )}
+      </motion.div>
+    </div>
+  );
+};
