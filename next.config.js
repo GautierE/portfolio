18d7633938a6517453b9d5228254ec8045bbe4af
Gautier <gautier.edel.info@gmail.com>
2024-06-13 20:15:50 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  redirects() {
+    return [
+    process.env.MAINTENANCE_MODE === "1"
+    ? { source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }
+    : null,
+    ].filter(Boolean);
+  }
 };
 
 module.exports = nextConfig;
